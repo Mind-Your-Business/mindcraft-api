39be6851f8be24c194ecf06071caf0cd31062073
Vanessa Mauna <vmauna@aol.com>
2019-11-25 11:05:05 -0500

    fix: updated index.js to try to remedy cors issue

--- server/index.js
+++ server/index.js
@@ -56,7 +56,7 @@ const createApp = () => {
   app.use(passport.initialize())
   app.use(passport.session())
   app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'https://mindcraft-api.herokuapp.com')
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept',
