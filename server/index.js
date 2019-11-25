6225a18b8fe35b66573cd7fba08a4d6ab6d7c9cd
Vanessa Mauna <vmauna@aol.com>
2019-11-24 22:10:00 -0500

    Updated index.js to allow for CORS

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
+
 module.exports = app
 
 // This is a global Mocha hook, used for resource cleanup.
@@ -62,7 +63,17 @@ const createApp = () => {
   )
   app.use(passport.initialize())
   app.use(passport.session())
-
+  app.use(function(req, res, next) {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://mindcraft-api.herokuapp.com'
+    )
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
   // auth and api routes
   app.use('/auth', require('./auth'))
   app.use('/api', require('./api'))
