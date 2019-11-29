0c2cbbc469b9eaa6db8cfd337bf06a727b415f03
Vanessa Mauna <vmauna@aol.com>
2019-11-29 12:27:57 -0500

    fix: Updated quizzes api routes

--- server/api/quizzes.js
+++ server/api/quizzes.js
@@ -1,25 +1,39 @@
 const router = require('express').Router()
-const {User, Quizzes, Quiz} = require('../db/models')
+const {Quizzes, Quiz} = require('../db/models')
+
 module.exports = router
 
-//get user by id:
 router.get('/', async (req, res, next) => {
   try {
-    const quizzes = await Quizzes.findAll({
-      include: {
-        model: Quiz
-      }
+    const quiz = await Quizzes.findAll({
+      attributes: ['name', 'id', 'questions']
     })
-    res.send(quizzes)
+    res.send(quiz)
   } catch (error) {
     next(error)
   }
 })
+
 router.get('/quiz/:id', async (req, res, next) => {
   try {
     const quizzes = await Quiz.findByPk(req.params.id)
     console.log('quiz', quizzes)
     res.send(quizzes)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/quiz/:id', async (req, res, next) => {
+  try {
+    let thisQuiz = await Quiz.findByPk(req.params.id)
+    const updatedInfo = {
+      correctAnswers: req.body.correctAnswers,
+      completedCorrectly: req.body.completedCorrectly,
+      incorrectAnswers: req.body.incorrectAnswers
+    }
+    thisQuiz = await thisQuiz.update(updatedInfo)
+    res.send(thisQuiz)
   } catch (error) {
     next(error)
   }
