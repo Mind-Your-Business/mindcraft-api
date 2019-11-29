7dce10e0524bef08d97f4463ad609aecb257825e
Vanessa Mauna <53280929+v-mauna@users.noreply.github.com>
2019-11-29 12:45:09 -0500

    Update quizzes.js

--- server/api/quizzes.js
+++ server/api/quizzes.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 const {User, Quizzes, Quiz} = require('../db/models')
 module.exports = router
 
-//get user by id:
 router.get('/', async (req, res, next) => {
   try {
     const quizzes = await Quizzes.findAll({
@@ -15,11 +14,27 @@ router.get('/', async (req, res, next) => {
     next(error)
   }
 })
+
 router.get('/quiz/:id', async (req, res, next) => {
   try {
-    const quizzes = await Quiz.findByPk(req.params.id)
-    console.log('quiz', quizzes)
-    res.send(quizzes)
+    const oneQuiz = await Quiz.findByPk(req.params.id)
+    console.log('quiz', oneQuiz)
+    res.send(oneQuiz)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/quiz/:id', async (req, res, next) => {
+  try {
+    let thisQuiz = await Quiz.findByPk(req.params.id)
+    const updatedInfo = {
+      correctAnswers: req.body.correctAnswers,
+      completedCorrectly: req.body.completedCorrectly,
+      incorrectAnswers: req.body.incorrectAnswers
+    }
+    thisQuiz = await thisQuiz.update(updatedInfo)
+    res.send(thisQuiz)
   } catch (error) {
     next(error)
   }
